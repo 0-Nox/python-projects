5aac7c3de59d23f649edfe3274bf95743588bf76
0-Nox <subhransupadhi40@gmail.com>
2024-11-21 14:31:43 +0530

    Random_num_guess.py

--- random_num_comp.py
+++ random_num_comp.py
@@ -1,8 +1,13 @@
+#Imports a inbuilt module called random
 import random
+#Taking the input from the user to set the maximum range value e.g 1-10 or 1-20
 x=int(input("Enter the max range of the number: "))
+#This is a function for the guess
 def guess(x):
+    #This is function in random module it selects a random number between (a,b) in this case its (1-x)
     random_number=random.randint(1,x)
     guess=0
+    #This a loop take input until the user gets the right number
     while guess != random_number:
         guess=int(input(f"Guess a number between 1 to {x}: "))
         if(guess==random_number):
